1502e58781a41bcfd989faa94bd102c0f5627f45
hyeonic <evan3566@gmail.com>
2021-03-21 15:23:03 +0900

    feat: UserController 수정 signupForm, userResult page 추가
    UserController 수정
    signupForm.html 추가
    userResult.html 추가

--- src/main/java/com/example/mystudy/controller/UserController.java
+++ src/main/java/com/example/mystudy/controller/UserController.java
@@ -2,25 +2,36 @@ package com.example.mystudy.controller;
 
 import com.example.mystudy.domain.User;
 import com.example.mystudy.repository.UserRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class UserController {
 
     private UserRepository userRepository = UserRepository.getInstance();
 
-    @PostMapping("/users")
-    public User signup(
+    @GetMapping("/users/signup")
+    public String signupForm(Model model) {
+        model.addAttribute("userForm", new UserForm());
+        return "user/signupForm";
+    }
+
+    @PostMapping("/users/signup")
+    public String signup(Model model,
             @RequestParam String email,
             @RequestParam String password,
             @RequestParam String name) {
-        Long savedUserId = userRepository.save(email, password, name);
 
-        return userRepository.findById(savedUserId);
+        Long savedUserId = userRepository.save(email, password, name);
+        UserForm userForm = new UserForm(email, password, name);
+
+        model.addAttribute("userForm", userForm);
+
+        return "user/userResult";
     }
 
     @GetMapping("/users/{userId}")
