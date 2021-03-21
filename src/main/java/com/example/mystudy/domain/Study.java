6dbf1263b228410fac78c361873ef38ec8beb394
hyeonic <evan3566@gmail.com>
2021-03-21 12:57:34 +0900

    feat: User, Study 도메인 추가
    User 도메인 추가
    Study 도메인 추가

---
+++ src/main/java/com/example/mystudy/domain/Study.java
@@ -0,0 +1,28 @@
+package com.example.mystudy.domain;
+
+public class Study {
+
+    private Long id;
+    private String name;
+
+    public Study(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
