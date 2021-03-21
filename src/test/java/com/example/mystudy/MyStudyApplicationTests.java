e21966d99c5ef4269bbd8322de8d48db033b08f3
hyeonic <evan3566@gmail.com>
2021-03-21 12:33:59 +0900

    feat: 프로젝트 초기 설정
    Spring boot: 2.4.4
    java version: 11
    build tool: gradle
    spring-boot-starter-web

---
+++ src/test/java/com/example/mystudy/MyStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mystudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
